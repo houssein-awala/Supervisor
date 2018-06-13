0651df5b1c99f9a07d0a3840bdb39793fe537886
mohye <mohyeddine-mohamad@hotmail.com>
2018-06-13 23:02:06 +0300

    ISensor Interface

---
+++ src/ISensor.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+/*
+ * this interface is the remote interface shared with supervisor to setup the sensor remotly
+ */
+public interface ISensor extends Remote {
+    public Descriptor getDescriptor();
+    public void configure(Descriptor descriptor);
+}
