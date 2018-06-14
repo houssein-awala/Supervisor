43b56b2ea2215921024cb8054b5f6b551d078c64
mohye <mohyeddine-mohamad@hotmail.com>
2018-06-14 22:13:12 +0300

    Synchronization

--- src/SupervisionParms.java
+++ src/SupervisionParms.java
@@ -8,18 +8,18 @@ import java.util.HashMap;
 */
 public class SupervisionParms
 {
-    // hashmap contains reference to all sensors to use with the simulator and first initialize of a sensor
+   // hashmap contains reference to all sensors to use with the simulator and first initialize of a sensor and its lock
     protected HashMap<String,ISensor> Sensors;
-
-    //hashmap contains descriptors of all sensors to use regularly
+    private Object senlock;
+   //hashmap contains descriptors of all sensors to use regularly and its lock
     protected HashMap<String,Descriptor> Descriptors;
-
-    //hashmap contains a timer for each sensor registered
+    private Object deslock;
+   //hashmap contains a timer for each sensor registered and its lock
     protected HashMap<String,Timer> Timers;
-
-    //Number of Request taken from Sink,may be considered the id of a request
+    private Object timlock;
+   //Number of Request taken from Sink,may be considered the id of a request and its lock
     protected int nbSinkRequests;
-
+    private Object reqlock;
     //How many chances given to a sensor to update its descriptor
     protected final int nbChances=3;
 
@@ -30,79 +30,88 @@ public class SupervisionParms
         Descriptors=new HashMap<>();
         Timers=new HashMap<>();
         nbSinkRequests=0;
+        senlock=new Object();
+        deslock=new Object();
+        timlock=new Object();
+        reqlock=new Object();
     }
 
     //Increase the number of requests by 1
     public void IncNbRequest()
     {
-        nbSinkRequests++;
+        synchronized (reqlock)
+        { nbSinkRequests++;}
     }
 
     //get the total number of request received from sink
     public int getNbSinkRequests() {
-        return nbSinkRequests;
+       synchronized (reqlock){return nbSinkRequests;}
     }
 
     // Getters and setters
     public HashMap<String, Descriptor> getDescriptors() {
-        return Descriptors;
+        synchronized (deslock){return Descriptors;}
     }
 
 
     public HashMap<String, ISensor> getSensors() {
-        return Sensors;
+       synchronized (senlock){return Sensors;}
     }
 
 
     //get sensor reference by ID
     public ISensor getSensor(String id)
     {
-        return Sensors.get(id);
+       synchronized (senlock){return Sensors.get(id);}
     }
 
     //add a sensor reference
     public void addSensor(String id,ISensor sensor)
     {
-        Sensors.put(id,sensor);
+       synchronized (senlock){Sensors.put(id,sensor);}
     }
 
     //get descriptor  by ID
     public Descriptor getDescriptor(String id)
     {
-        return Descriptors.get(id);
+       synchronized (deslock){return Descriptors.get(id);}
     }
 
     //add a descriptor
     public void addDescriptor(Descriptor descriptor)
     {
-        Descriptors.put(descriptor.getId(),descriptor);
+       synchronized (deslock){Descriptors.put(descriptor.getId(),descriptor);}
     }
 
 
     //add a timer and start it
     public void addTimer(String id,Timer timer)
     {
-        Timers.put(id,timer);
-        Timers.get(id).start();
+        synchronized (timlock) {
+            Timers.put(id, timer);
+            Timers.get(id).start();
+        }
     }
 
     //get a timer if it exists,else return null
     public Timer getTimer(String id)
     {
-        return Timers.get(id);
+       synchronized (timlock){return Timers.get(id);}
     }
 
     //delete a timer if it exists
     public void deleteTimer(String id)
     {
-        if(Timers.containsKey(id))
-            Timers.remove(id);
+        synchronized (timlock) {
+            if (Timers.containsKey(id))
+                Timers.remove(id);
+        }
     }
 
     //Check if a timer exist
     public boolean isTimerExist(String id)
     {
-        return Timers.containsKey(id);
+       synchronized (timlock){return Timers.containsKey(id);}
     }
 
     //get number of chances
