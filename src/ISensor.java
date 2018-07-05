14e797578c81df88393af7578bfa86ee70104896
LENOVO <ghinasaad12@gmail.com>
2018-07-05 12:59:19 -0700

    commit interface+rmi_Ifactory

--- src/ISensor.java
+++ src/ISensor.java
@@ -5,7 +5,11 @@ import java.rmi.Remote;
  */
 public interface ISensor extends Remote {
     public Descriptor getDescriptor();
+
     public void configure(Descriptor descriptor);
-    public void editSensor(String id,int type,Point position);
-    public  void deleteSensor(String id);
+
+    public void editSensor(String id, int type, Point position,int state);
+
+    public void deleteSensor(String id);
+
 }
